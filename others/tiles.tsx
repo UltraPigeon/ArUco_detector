44193c85c13d0c9b7972908003032de1cc65206d
UltraPigeon <xazmat1138@gmail.com>
2023-06-17 21:34:41 +0300

    add tiles.tsx for tailed

---
+++ others/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tiles" tilewidth="10" tileheight="10" tilecount="12" columns="3">
+ <image source="tiles.png" width="34" height="40"/>
+</tileset>
